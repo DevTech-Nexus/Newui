3506309e6f57ab07ea72db5ba63a0e7966535d50
Malinhara <poornamalinhara53@gmail.com>
2023-10-22 14:08:49 +0530

    poorna

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+import About from './Components/About';
+import Cart from './Components/Cart';
+import Dashboard from './Components/Dashboard';
+import Foot from './Components/Foot';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Navigationbar from './Components/Navigationbar';
+import User from './Components/Orderstatus';
+import Register from './Components/Register';
+import Shop from './Components/Shop';
+import './Components/Styles.css';
+import Update from './Components/Update';
+function App() {
+  
+  return (
+
+    
+    <div>
+
+  
+<Router>
+  
+
+  <Navigationbar/>
+  
+<Routes>
+<Route exact path='/' element={<Home/>}></Route>
+      <Route exact path='/Home' element={<Home/>}></Route>
+      <Route exact path='/Shop' element={<Shop/>}></Route>
+      <Route exact path='/About' element={<About/>}></Route>
+      <Route exact path='/Login' element={<Login/>}></Route>
+      <Route exact path='/Cart' element={<Cart/>}></Route>
+      <Route exact path='/Dashboard' element={<Dashboard/>}></Route>
+      <Route exact path='/Register' element={<Register/>}></Route>
+      <Route exact path='/Login' element={<Login/>}></Route>
+      <Route exact path='/Orderstatus' element={<User/>}></Route>
+      <Route exact path='/Update' element={<Update/>}></Route>
+     
+     
+      </Routes>
+      <Foot/>
+      
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navigationbar.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { useLocation } from "react-router-dom";
+
+import './Styles.css';
+
+export default function Navigationbar(){
+  const location = useLocation();
+
+  // Hide navbar on the login page and Login page
+  if (location.pathname === '/Dashboard' || location.pathname === '/Login') {
+    return null;
+  }
+  else if (location.pathname === '/Register' || location.pathname === '/Login') {
+    return null;
+  }
+  
+
+  return (
+    <div>
+      <Navbar expand="lg" bg="dark" data-bs-theme="dark" fixed="top">
+        <Container fluid>
+          <Navbar.Brand style={{ color: '#FFFFFF' }} href="#" className='logo'>Expert Mobile</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link className='Links' style={{ color: '#FFFFFF' }} href="/Home">Home</Nav.Link>
+              <Nav.Link className='Links' style={{ color: '#FFFFFF' }} href="/Shop">Shop</Nav.Link>
+              <Nav.Link className='Links' style={{ color: '#FFFFFF' }} href="/About">About</Nav.Link>
+            </Nav>
+
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+
+            {/* Add user dropdown menu with custom CSS */}
+            <Dropdown align="end" style={{ marginLeft: '10px' }}>
+              <Dropdown.Toggle variant="secondary" id="user-dropdown">
+                User
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item href="/Login">Login</Dropdown.Item>
+                <Dropdown.Item href="/Register">Register</Dropdown.Item>
+                <Dropdown.Item href="/Register">Log out</Dropdown.Item>
+                <Dropdown.Item href="/Orderstatus">Order Status</Dropdown.Item>
+                <Dropdown.Item href="/Update">Edit Profile</Dropdown.Item>
+
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <a href='/Cart'>
+              <img
+                className='cart'
+                src="https://th.bing.com/th/id/R.22a468f2a23252fe251834f6fa559645?rik=NMt%2fVFuJ%2fdJV2A&riu=http%3a%2f%2fwww.newdesignfile.com%2fpostpic%2f2010%2f06%2fshopping-cart-icon-white_235863.png&ehk=iOQPHRjP2Yr4csqVdX6Bs7D8GFTrr2r9icBq9odkO7I%3d&risl=&pid=ImgRaw&r=0"
+                alt="Shopping Cart"
+              />
+            </a>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
