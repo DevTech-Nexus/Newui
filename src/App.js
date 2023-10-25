559ea813196169c0b39035f463fcede97eecba54
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-25 18:14:00 +0530

    added title and favicon

--- src/App.js
+++ src/App.js
@@ -14,35 +14,35 @@ import Shop from './Components/Shop';
 import './Components/Styles.css';
 import Update from './Components/Update';
 function App() {
-  
+
   return (
 
-    
+
     <div>
 
-  
-<Router>
-  
 
-  <Navigationbar/>
-  
-<Routes>
-<Route exact path='/' element={<Home/>}></Route>
-      <Route exact path='/Home' element={<Home/>}></Route>
-      <Route exact path='/Shop' element={<Shop/>}></Route>
-      <Route exact path='/About' element={<About/>}></Route>
-      <Route exact path='/Login' element={<Login/>}></Route>
-      <Route exact path='/Cart' element={<Cart/>}></Route>
-      <Route exact path='/Dashboard' element={<Dashboard/>}></Route>
-      <Route exact path='/Register' element={<Register/>}></Route>
-      <Route exact path='/Login' element={<Login/>}></Route>
-      <Route exact path='/Orderstatus' element={<User/>}></Route>
-      <Route exact path='/Update' element={<Update/>}></Route>
-     
-     
-      </Routes>
-      <Foot/>
-      
+      <Router>
+
+
+        <Navigationbar />
+
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route exact path='/Home' element={<Home />}></Route>
+          <Route exact path='/Shop' element={<Shop />}></Route>
+          <Route exact path='/About' element={<About />}></Route>
+          <Route exact path='/Login' element={<Login />}></Route>
+          <Route exact path='/Cart' element={<Cart />}></Route>
+          <Route exact path='/Dashboard' element={<Dashboard />}></Route>
+          <Route exact path='/Register' element={<Register />}></Route>
+          <Route exact path='/Login' element={<Login />}></Route>
+          <Route exact path='/Orderstatus' element={<User />}></Route>
+          <Route exact path='/Update' element={<Update />}></Route>
+
+
+        </Routes>
+        <Foot />
+
       </Router>
     </div>
   );
