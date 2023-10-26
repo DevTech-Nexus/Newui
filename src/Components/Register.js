977340be950c3f2fe98670a701507d1b0022b4fa
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-26 10:37:06 +0530

    refactoring and optimized imports in register

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,96 +1,94 @@
 import {
-    MDBBtn,
-    MDBCol,
-    MDBContainer,
-    MDBIcon,
-    MDBInput,
-    MDBRow
+  MDBCol,
+  MDBContainer,
+  MDBInput,
+  MDBRow
 } from 'mdb-react-ui-kit';
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-  
-  import './Styles.css';
-  
-  
-  
-   export default function Register() {
-     
-   
-  
-    const [formData, setFormData] = useState({
-      email: '',
-      password: '',
-      user: '',
-    });
-  
-    const [errors, setErrors] = useState({
-      email: '',
-      password: '',
-      user: '',
-    });
-  
-    
-    
-  
-    const handleInputChange = (e) => {
-      const { name, value } = e.target;
-      setFormData({ ...formData, [name]: value });
-    };
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      
-      // Validate email and password fields
-      const validationErrors = {};
 
-      if (!formData.user) {
-        validationErrors.user = 'username is required';
-      }
-      
-      
-      if (!formData.email) {
-        validationErrors.email = 'Email is required';
-      }
-      
-      if (!formData.password) {
-        validationErrors.password = 'Password is required';
-      }
-  
-      // Set validation errors if any
-      setErrors(validationErrors);
-  
-      // If no validation errors, you can proceed with form submission logic
-      if (Object.keys(validationErrors).length === 0) {
-        // Add your logic here, e.g., send the form data to a server
+import './Styles.css';
 
 
-        
-      
-      }
-    };
-  
-  
-   
-  
-    return (
-  
-  
-      <div>
+
+export default function Register() {
+
+
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    user: '',
+  });
+
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+    user: '',
+  });
+
+
+
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate email and password fields
+    const validationErrors = {};
+
+    if (!formData.user) {
+      validationErrors.user = 'username is required';
+    }
+
+
+    if (!formData.email) {
+      validationErrors.email = 'Email is required';
+    }
+
+    if (!formData.password) {
+      validationErrors.password = 'Password is required';
+    }
+
+    // Set validation errors if any
+    setErrors(validationErrors);
+
+    // If no validation errors, you can proceed with form submission logic
+    if (Object.keys(validationErrors).length === 0) {
+      // Add your logic here, e.g., send the form data to a server
+
+
+
+
+    }
+  };
+
+
+
+
+  return (
+
+
+    <div>
       <MDBContainer fluid className="p-3 my-5">
-  
+
         <MDBRow>
-  
+
           <MDBCol col='10' md='6'>
             <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="img-fluid"
-             alt="Phone imagefd" />
+              alt="Phone imagefd" />
           </MDBCol>
-  
-          <MDBCol col='4' md='6'>
-  
-  
-          <form onSubmit={handleSubmit}>
 
-          <MDBInput
+          <MDBCol col='4' md='6'>
+
+
+            <form onSubmit={handleSubmit}>
+
+              <MDBInput
                 wrapperClass='mb-4'
                 label='username'
                 id='formControlLg'
@@ -101,7 +99,7 @@ import Button from 'react-bootstrap/Button';
                 onChange={handleInputChange}
               />
 
-                  {errors.user && <div className="text-danger">{errors.user}</div>}
+              {errors.user && <div className="text-danger">{errors.user}</div>}
               <MDBInput
                 wrapperClass='mb-4'
                 label='Email address'
@@ -113,7 +111,7 @@ import Button from 'react-bootstrap/Button';
                 onChange={handleInputChange}
               />
               {errors.email && <div className="text-danger">{errors.email}</div>}
-  
+
               <MDBInput
                 wrapperClass='mb-4'
                 label='Password'
@@ -125,57 +123,57 @@ import Button from 'react-bootstrap/Button';
                 onChange={handleInputChange}
               />
               {errors.password && <div className="text-danger">{errors.password}</div>}
-  
-              
-             
-              
+
+
+
+
               <a href='/register'><Button
                 variant="primary"
                 size="lg"
                 type="submit"
-               
+
                 style={{
-                    marginRight: '10px' ,
+                  marginRight: '10px',
                   background:
                     'linear-gradient(to right, rgba(101, 126, 234, 0.9), rgba(118, 75, 162, 0.9))',
                 }}
-               
+
                 className="custom-button2"
-                // Add type="submit" to the button
+              // Add type="submit" to the button
               >
-             Register
+                Register
               </Button></a>
 
-              <br/>
-              <br/>
+              <br />
+              <br />
               <a href='/login'><Button
-              variant="primary"
-              size="md"
-             
-              style={{
-               
-                background:
-                  'linear-gradient(to right, rgba(118, 75, 162, 0.7, rgba(101, 126, 234, 0.7)',
-              }}
-             
-              className="custom-button2"
+                variant="primary"
+                size="md"
+
+                style={{
+
+                  background:
+                    'linear-gradient(to right, rgba(118, 75, 162, 0.7, rgba(101, 126, 234, 0.7)',
+                }}
+
+                className="custom-button2"
               // Add type="submit" to the button
-            >
-            Back to login
-            </Button></a>
-              </form>
-              
-  
-  
+              >
+                Back to login
+              </Button></a>
+            </form>
+
+
+
             {/* <div className="d-flex justify-content-between mx-4 mb-4">
               <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
               <a href="!#">Forgot password?</a>
             </div> */}
-  
-            
-  
+
+
+
           </MDBCol>
-  
+
         </MDBRow>
         {/* <Routes>
        
@@ -184,7 +182,6 @@ import Button from 'react-bootstrap/Button';
        
         </Routes> */}
       </MDBContainer></div>
-    );
-  }
-  
-  
+  );
+}
+
