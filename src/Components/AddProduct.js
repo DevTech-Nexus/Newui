a58e0ecd6a496d83961f51fb2dc8db4d5318f271
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-27 22:56:16 +0530

    fixed up add product page

--- src/Components/AddProduct.js
+++ src/Components/AddProduct.js
@@ -16,46 +16,53 @@ const ProductAddPage = () => {
       <br /><br /><br /><br />
       <center> <form>
 
-        <div class="row mb-4">
-          <div class="col">
+
+          <div class="form-outline mb-4">
+            <input type="text" id="form6Example2" class="form-control" name='productName'/>
+            <label class="form-label" for="productName">Product Name</label>
           </div>
-          <div class="col">
-            <div class="form-outline">
-              <input type="text" id="form6Example2" class="form-control" />
-              <label class="form-label" for="form6Example2">Product Name</label>
-            </div>
-          </div>
+
+        <div class="form-outline mb-4">
+          <input type="number" id="form6Example6" class="form-control" name='price'/>
+          <label class="form-label" for="price">Product Price</label>
         </div>
 
 
         <div class="form-outline mb-4">
-          <input type="text" id="form6Example3" class="form-control" />
-          <label class="form-label" for="form6Example3">Product Title</label>
+          <textarea class="form-control" id="form6Example7" rows="4" name='description'></textarea>
+          <label class="form-label" for="description">Description</label>
         </div>
 
-
-
-
-
-
-
+      <hr/>
 
         <div class="form-outline mb-4">
-          <input type="number" id="form6Example6" class="form-control" />
-          <label class="form-label" for="form6Example6">Product Price</label>
+          <input type="text" id="form6Example3" class="form-control" name='category'/>
+          <label class="form-label" for="category">Category</label>
         </div>
-
 
         <div class="form-outline mb-4">
-          <textarea class="form-control" id="form6Example7" rows="4"></textarea>
-          <label class="form-label" for="form6Example7">Product Description</label>
+          <input type="text" id="form6Example3" class="form-control" name='brand'/>
+          <label class="form-label" for="brand">Brand</label>
+        </div>
+
+        <div class="form-outline mb-4">
+          <input type="text" id="form6Example3" class="form-control" name='weight'/>
+          <label class="form-label" for="weight">Weight</label>
+        </div>
+
+        <div class="form-outline mb-4">
+          <input type="text" id="form6Example3" class="form-control" name='imgUrl'/>
+          <label class="form-label" for="imgUrl">Image URL</label>
+        </div>
+
+        <div class="form-outline mb-4">
+          <input type="text" id="form6Example3" class="form-control" name='dimensions'/>
+          <label class="form-label" for="dimensions">Dimensions (LxWxH)</label>
         </div>
 
 
 
-
-
-        <button type="submit" class="btn btn-primary btn-block mb-4">Add Recorde</button>
+        <button type="submit" class="btn btn-primary btn-block mb-4">Add Device</button>
       </form></center>
 
     </div>
